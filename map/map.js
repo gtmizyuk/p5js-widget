af46bd5d98e8ee4e21384364d68d21e86ca94c37
Oleksandr Miziuk <gtmizyuk@gmail.com>
2023-07-23 20:55:01 +0300

    improvement of the map

--- map/map.js
+++ map/map.js
@@ -1,33 +1,60 @@
-let t, cnv, theta, controlBoard;
-let sliderShapeTurtle, sliderSizeTurtle, sliderHeadingTurtle, sliderPenSize, colorPicker, btnSaveCanvasTurtle, btnSaveCoordsTurtle;
+let t, tMirror, cnv, theta, controlBoard, gridPrivate;
+let sliderShapeTurtle,
+  sliderSizeTurtle,
+  sliderHeadingTurtle,
+  sliderPenSize,
+  colorPicker,
+  btnSaveCanvasTurtle,
+  btnSaveCoordsTurtle;
 let checkItems = {};
-let coords = [];
+let coords = []; // координати для текстового файлу
 let coordsTitle = false;
+let startTurtleX, startTurtleY;
+let progress = []; // стани Черепашки
 
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
   cnv.mousePressed(mClick);
-  
+
   colorPicker = createColorPicker("#ed225d"); // Cerise
   colorPicker.position(10, 220);
 
   controlBoard = createGraphics(windowWidth, windowHeight);
 
-  // примірник Черепашки
+  gridPrivate = createGraphics(width, height);
+
+  // основний примірник Черепашки
   t = new Turtle();
 
-  // мапа
-  t.grid(50, color(0, 50), color(11, 114, 159)); // Cerulean
+  [startTurtleX, startTurtleY] = t.getPosition();
+  t.setColor(colorPicker.color(), colorPicker.color());
+  t.write(`(${startTurtleX}, ${startTurtleY})`, {
+    horizontal: LEFT,
+    vertical: BOTTOM,
+  });
 
-  // слайдери 
-  createSliders();  
-  
-  // чекбокси 
+  // невидимий примірник Черапашки для роботи за лаштунками
+  tMirror = new Turtle();
+  tMirror.penColor = color(255, 0);
+
+  // створити сітку
+  createGrid(
+    (step = 50),
+    (colorLines = color(0, 50)),
+    (colorAxis = color(11, 114, 159)),
+    (linesThickness = 1),
+    (axisThickness = 2)
+  );
+
+  // створити слайдери
+  createSliders();
+
+  // створити чекбокси
   createCheckBoxes();
 
-  // кнопки
-  // зберегти зображeння
-  btnSaveCanvasTurtle = createButton("зберегти полотно");
+  // ствторити кнопки:
+  // зберегти зображeння у форматі PNG
+  btnSaveCanvasTurtle = createButton("зберегти полотно png");
   btnSaveCanvasTurtle.mousePressed(saveCanvasTurtle);
   btnSaveCanvasTurtle.position(10, 275);
   btnSaveCanvasTurtle.addClass("btn");
@@ -39,26 +66,91 @@ function setup() {
 }
 
 function draw() {
-  background(255, 0);
-  interfaceItems(); 
-  t.place(); 
-  image(controlBoard, 0, 0, controlBoard.width, controlBoard.height); 
+  t.place();
+  interfaceItems();
+  image(controlBoard, 0, 0, controlBoard.width, controlBoard.height);
 }
 
-function mClick() {  
-  if (mouseIsPressed) {
-    if (mouseButton === LEFT) {
-      let [turtleX, turtleY] = t.calculateMouseXY(mouseX, mouseY);
-      t.setPosition(turtleX, turtleY);
-      if (coordsTitle) {
-        t.write(`(${turtleX}, ${turtleY})`, { horizontal: LEFT, vertical: BOTTOM });
-      }
-      coords.push([turtleX, turtleY]);
-    }
+function createGrid(
+  step = 50,
+  colorLines = color(0, 50),
+  colorAxis = color(11, 114, 159),
+  linesThickness = 1,
+  axisThickness = 2
+) {
+  // сітка
+  gridPrivate.stroke(colorLines);
+  gridPrivate.strokeWeight(linesThickness);
+  for (let i = 0; i < gridPrivate.width / 10; i++) {
+    gridPrivate.line(i * step, 0, i * step, gridPrivate.height);
   }
+  for (let j = 0; j < gridPrivate.height / 10; j++) {
+    gridPrivate.line(0, j * step, gridPrivate.width, j * step);
+  }
+  // центральні лінії
+  gridPrivate.stroke(colorAxis); // за замовчуванням Cerulean
+  gridPrivate.strokeWeight(axisThickness);
+  gridPrivate.line(
+    gridPrivate.width / 2,
+    0,
+    gridPrivate.width / 2,
+    gridPrivate.height
+  );
+  gridPrivate.line(
+    0,
+    gridPrivate.height / 2,
+    gridPrivate.width,
+    gridPrivate.height / 2
+  );
 }
 
-function createSliders(){
+function mClick() {
+  // по кліку малюємо Черепашку, її координати та фіксуємо кожен її стан
+  t.setShape(t.shapes[sliderShapeTurtle.value()]);
+  t.setShapeSize(sliderSizeTurtle.value());
+  t.setHeading(theta);
+  t.setPenSize(sliderPenSize.value());
+  t.setColor(colorPicker.color());
+  t.setFillColor(colorPicker.color());
+
+  let [turtleX, turtleY] = t.calculateMouseXY(mouseX, mouseY);
+  t.setPosition(turtleX, turtleY);
+
+  tMirror.setPosition(turtleX, turtleY);
+
+  if (coordsTitle) {
+    t.write(`(${turtleX}, ${turtleY})`, { horizontal: LEFT, vertical: BOTTOM });
+  }
+  coords.push([turtleX, turtleY]);
+
+  let startState = {
+    turtleShape: 3,
+    turtleSize: 1,
+    turtleHeading: 0,
+    penSize: 1,
+    colorTurtlePen: "rgba(237, 34, 93, 1)",
+    turtleX: 0,
+    turtleY: 0,
+    coordsTitle: true,
+    penIsUp: false,
+  };
+  progress.splice(0, 1, startState);
+
+  let currentState = {
+    turtleShape: sliderShapeTurtle.value(),
+    turtleSize: sliderSizeTurtle.value(),
+    turtleHeading: theta,
+    penSize: sliderPenSize.value(),
+    colorTurtlePen: colorPicker.color(),
+    turtleX: turtleX,
+    turtleY: turtleY,
+    coordsTitle: coordsTitle,
+    penIsUp: t.penIsUp,
+  };
+  progress.push(currentState);
+}
+
+function createSliders() {
   // створення слайдерів:
   // для зміни форми Черепашки
   sliderShapeTurtle = createSlider(0, t.shapes.length - 1, 3, 1);
@@ -81,89 +173,189 @@ function createSliders(){
   sliderPenSize.style("width", "130px");
 }
 
-function slidersHeadlines(){
+function slidersHeadlines() {
   // текстові заголовки для слайдерів
   controlBoard.textAlign(LEFT, CENTER);
   controlBoard.noStroke();
   controlBoard.fill(254, 185, 95); // Hunyadi yellow
-  controlBoard.stroke(235, 235, 235); // Anti-flash white 
+  controlBoard.stroke(235, 235, 235); // Anti-flash white
   controlBoard.rect(-1, t.heightDashboard + 10, 155, 295, 0, 4, 4, 0);
   controlBoard.noStroke();
   controlBoard.fill(97, 112, 125, 255); // Payne's gray
-   
+
   controlBoard.text(`форма: ${t.shapes[sliderShapeTurtle.value()]}`, 10, 50);
-  t.setShape(t.shapes[sliderShapeTurtle.value()]);
+  //t.setShape(t.shapes[sliderShapeTurtle.value()]);
 
   controlBoard.text(`розмір: ${sliderSizeTurtle.value()}`, 10, 90);
-  t.setShapeSize(sliderSizeTurtle.value());
+  //t.setShapeSize(sliderSizeTurtle.value());
 
   theta = sliderHeadingTurtle.value() === 360 ? 0 : sliderHeadingTurtle.value();
-  controlBoard.text(`кутова орієнтація: ${theta}\u00B0`, 10, 130);  
-  t.setHeading(theta);
-  
+  controlBoard.text(`кутова орієнтація: ${theta}\u00B0`, 10, 130);
+  tMirror.setHeading(theta);
+
   controlBoard.text(`товщина олівця: ${sliderPenSize.value()}`, 10, 170);
-  t.setPenSize(sliderPenSize.value());
+  //t.setPenSize(sliderPenSize.value());
 
   controlBoard.text(`колір: ${colorPicker.color()}`, 10, 210);
-  t.setColor(colorPicker.color());  
+  //t.setColor(colorPicker.color());
+  //t.setFillColor(colorPicker.color());
   controlBoard.fill(255, 255);
 }
 
-function createCheckBoxes(){
+function createCheckBoxes() {
   // чекбокс для вимкнення/увімкнення:
   // олівця
-  checkItems["pen"] = createCheckbox("олівець", true);
+  checkItems.pen = createCheckbox("олівець", true);
   checkItems.pen.changed(checkedVisiblePen);
   checkItems.pen.position(10, 250);
   checkItems.pen.addClass("chk");
 
   // написи для координат
-  checkItems["coords"] = createCheckbox("написи", false);
+  checkItems.coords = createCheckbox("написи", false);
   checkItems.coords.changed(checkedVisibleCoords);
   checkItems.coords.position(75, 250);
   checkItems.coords.addClass("chk");
+
+  // сітки, за замовчуванням вимкнена
+  checkItems.gridPrivate = createCheckbox("сітка", false);
+  checkItems.gridPrivate.changed(checkedVisibleGrid);
+  checkItems.gridPrivate.position(75, 225);
+  checkItems.gridPrivate.addClass("chk");
 }
 
 function checkedVisiblePen() {
-  // перемикання режимів олівця
+  // олівець на полотні?
   if (checkItems.pen.checked()) {
     t.penIsUp = false;
+    tMirror.penIsUp = false;
   } else {
-    t.penIsUp = true;    
+    t.penIsUp = true;
+    tMirror.penIsUp = true;
   }
 }
 
 function checkedVisibleCoords() {
-  // перемикання відображення написів координат на полотні
+  // чи увімкнено написи координат на полотні?
   if (checkItems.coords.checked()) {
     coordsTitle = true;
   } else {
-    coordsTitle = false;  
+    coordsTitle = false;
   }
 }
 
-function interfaceItems(){
+function checkedVisibleGrid() {
+  // чи увімкнено сітку?
+  if (checkItems.gridPrivate.checked()) {
+    image(gridPrivate, 0, 0, gridPrivate.width, gridPrivate.height);
+  } else {
+    clear();
+    memoryProgress();
+    image(gridPrivate, gridPrivate.width, gridPrivate.height);
+  }
+}
+
+function memoryProgress() {
+  // відновити кожен стан Черепашки, коли сітка вимикається
+  if (progress.length > 0) {
+    let statePrev = progress[0];
+    stateMemory(
+      statePrev.turtleShape,
+      statePrev.turtleSize,
+      statePrev.turtleHeading,
+      statePrev.penSize,
+      statePrev.colorTurtlePen,
+      0,
+      0,
+      statePrev.turtleX,
+      statePrev.turtleY,
+      statePrev.coordsTitle,
+      statePrev.penIsUp
+    );
+    for (let i = 1; i < progress.length; i++) {
+      let state = progress[i];
+      stateMemory(
+        state.turtleShape,
+        state.turtleSize,
+        state.turtleHeading,
+        state.penSize,
+        state.colorTurtlePen,
+        state.turtleX,
+        state.turtleY,
+        statePrev.turtleX,
+        statePrev.turtleY,
+        state.coordsTitle,
+        state.penIsUp
+      );
+      statePrev = progress[i];
+    }
+  } else {
+    t.write(`(${startTurtleX}, ${startTurtleY})`, {
+      horizontal: LEFT,
+      vertical: BOTTOM,
+    });
+  }
+}
+
+function stateMemory(
+  turtleShape,
+  turtleSize,
+  turtleHeading,
+  penSize,
+  colorTurtlePen,
+  turtleX,
+  turtleY,
+  turtleXPrev,
+  turtleYPrev,
+  coordsTitle,
+  penIsUp
+) {
+  // відновити місце, де була Черепашка, та її властивості
+  t.setShape(t.shapes[turtleShape]);
+  t.setShapeSize(turtleSize);
+  t.setHeading(turtleHeading);
+  t.setPenSize(penSize);
+  t.setColor(colorTurtlePen);
+  [t.tX, t.tY] = t.calculateTurtleXY();
+  noFill();
+  push();
+  translate(width / 2, height / 2);
+  scale(1, -1);
+  if (!penIsUp) {
+    stroke(colorTurtlePen);
+    strokeWeight(penSize);
+    line(turtleXPrev, turtleYPrev, turtleX, turtleY);
+  }
+  t.x = turtleX;
+  t.y = turtleY;
+  [t.x, t.y] = t.calculateTurtleXYnormal();
+  pop();
+  t.place();
+  if (coordsTitle) {
+    t.write(`(${turtleX}, ${turtleY})`, { horizontal: LEFT, vertical: BOTTOM });
+  }
+}
+
+function interfaceItems() {
   // елементи інтерфейсу
-  t.dashboard();
-  t.compass();
-  slidersHeadlines(); 
+  tMirror.dashboard();
+  tMirror.compass();
+  slidersHeadlines();
   checkedVisiblePen();
   checkedVisibleCoords();
 }
 
-function windowResized(){
-  //resizeCanvas(windowWidth, windowHeight);  
+function windowResized() {
+  //resizeCanvas(windowWidth, windowHeight);
 }
 
 function saveCanvasTurtle() {
   // зберегти зображення полотна у файл
-  save(cnv, "turtle.png");
+  saveCanvas(cnv, "turtle", "png");
 }
 
 function saveCoordsTurtle() {
   // зберегти координати Черепашки у текстовий файл
-  let content = "Координати відсутні, спочатку поклікайте мишкою на полотні.";
-  content = "0,0\r\n";
+  let content = "0,0\r\n";
   for (const co of coords) {
     content += `${co}\r\n`;
   }
@@ -174,4 +366,3 @@ function saveCoordsTurtle() {
   link.click();
   URL.revokeObjectURL(link.href);
 }
-
